479d49f725d4b540c49e3ef6faf7d3c94664821c
J. Moldawski <yevgen.moldawski@interseroh.com>
2019-06-03 18:03:49 +0200

    test with karma

---
+++ karma-ci.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+	"use strict";
+
+	/* eslint-disable no-unused-vars*/
+	require("./karma.conf")(config);
+	/* eslint-enable no-unused-vars*/
+
+	config.set({
+
+		browsers: ['ChromeHeadless'],
+
+		singleRun: true
+
+	});
+};
